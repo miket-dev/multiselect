1f59ddc1d87191cced211a6200962e2879f853a2
Mikhail Neofitov <m_neo2@mail.ru>
2016-08-02 17:31:17 +0500

    some code improvements

--- scripts/multiselect.js
+++ scripts/multiselect.js
@@ -1,7 +1,8 @@
 ﻿//multiselect collection
 window.multiselects = [];
-//jQuery extension for multiselect
+
 if (typeof ($) != 'undefined') {
+	//jQuery extension for multiselect
 	$.fn.multiselect = function () {
 		//if no elements passed - it is not an error
 		if (this.length != 0) {
@@ -15,8 +16,10 @@ if (typeof ($) != 'undefined') {
 		hideMultiselects(event);
 	});
 } else {
-	document.multiselect = function(e) {
-		window.multiselects.push(new Multiselect(e));
+	document.multiselect = function(selector) {
+		m_helper.each(document.querySelectorAll(selector), function(e) {
+			window.multiselects.push(new Multiselect(e));
+		});
 	}
 	
 	window.onclick = function(event) {
@@ -25,19 +28,10 @@ if (typeof ($) != 'undefined') {
 }
 
 function hideMultiselects(event) {
-	for (var i = 0; i < window.multiselects.length; i++) {
-		var e = window.multiselects[i];
-		if (document.getElementById(e._getItemListIdentifier()).offsetParent) {
-			var parent = event.target.parentElement;
-			while (parent && parent.tagName != 'BODY') {
-				if (parent.id == e._getIdentifier()) {
-					return;
-				}
-				
-				parent = parent.parentElement;
-			}			
-
+	m_helper.each(window.multiselects, function(e) {
+		if (document.getElementById(e._getItemListIdentifier()).offsetParent &&
+				!m_helper.parent(event.target, e._getIdentifier())) {
 			e._hideList(e, event);
 		}
-	}
+	});
 }
